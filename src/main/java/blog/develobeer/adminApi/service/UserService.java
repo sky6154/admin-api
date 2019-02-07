baf767c09b383e9771199f5aeea6b03408c891eb
kokj <kokj@neowiz.com>
2019-02-07 14:39:15 +0900

    save => saveAndFlush

--- src/main/java/blog/develobeer/adminApi/service/UserService.java
+++ src/main/java/blog/develobeer/adminApi/service/UserService.java
@@ -78,6 +78,6 @@ public class UserService implements UserDetailsService {
     }
 
     public User save(User user) {
-        return userRepository.save(user);
+        return userRepository.saveAndFlush(user);
     }
 }
