708f680bc555c9da34cbf126b34fb15878bdb88c
kokj <kokj@neowiz.com>
2019-04-19 17:21:43 +0900

    CORS * 일 경우 해당 origin을 allow하도록 변경

--- src/main/java/blog/develobeer/adminApi/config/CorsFilter.java
+++ src/main/java/blog/develobeer/adminApi/config/CorsFilter.java
@@ -1,23 +1,33 @@
 package blog.develobeer.adminApi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+    @Value("${spring.origin}")
+    private String allowOrigin;
+
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        if(allowOrigin.equals("*")){
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+        }
+        else{
+            response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-//        response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "X-Develobeer-Token, Content-Type, Access-Control-Allow-Headers");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
