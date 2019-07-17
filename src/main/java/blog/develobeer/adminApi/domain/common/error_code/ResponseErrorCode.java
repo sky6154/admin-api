fa64b1928f504073422a7309324bdd155acaacb6
sky6154 <sky6154@gmail.com>
2019-07-17 12:52:46 +0900

    불필요 기능 삭제

--- src/main/java/blog/develobeer/adminApi/domain/common/error_code/ResponseErrorCode.java
+++
@@ -1,9 +0,0 @@
-package blog.develobeer.adminApi.domain.common.error_code;
-
-public class ResponseErrorCode {
-    public static class Post{
-        public static final int FILE_NOT_EXIST = -1001;
-        public static final int DESTINATION_FOLDER_CREATION_FAIL = -1002;
-        public static final int UPLOAD_FAIL = -1003;
-    }
-}
