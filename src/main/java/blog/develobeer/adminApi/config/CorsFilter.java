420dc32a13ebd296fa49e22c2b893383b08b8a0b
sky6154 <sky6154@gmail.com>
2019-11-03 18:56:07 +0900

    cors url 변경

--- src/main/java/blog/develobeer/adminApi/config/CorsFilter.java
+++ src/main/java/blog/develobeer/adminApi/config/CorsFilter.java
@@ -13,9 +13,6 @@ import java.io.IOException;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
-    @Value("${spring.origin}")
-    private String allowOrigin;
-
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
@@ -24,7 +21,7 @@ public class CorsFilter implements Filter {
             response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
         }
         else{
-            response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+            response.setHeader("Access-Control-Allow-Origin", "https://admin.develobeer.blog");
         }
 
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
